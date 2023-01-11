7cef94f443d8a9184aabfa0dad901d01f30fb575
dr4g0n100 <74046398+dr4g0n100@users.noreply.github.com>
2023-01-11 21:12:05 +0800

    fixed backup with images

--- pages/backup.php
+++ pages/backup.php
@@ -117,72 +117,119 @@
 
 <?php
 
-$get_all_table_query = "SHOW TABLES";
-$statement = $connectPDO->prepare($get_all_table_query);
-$statement->execute();
-$result = $statement->fetchAll();
-
-
- $output = '';
-
  if(isset($_POST['TxtFileName']))
 {
-  $TxtFileName = $_POST['TxtFileName'];
-  $output.= "\n\nDROP DATABASE if exists clinicRecord;\n";
-  $output.= "CREATE DATABASE clinicRecord;\n";
-  $output.= "USE clinicRecord;\n";
 
-   $tableArray = array("archivedconsultation","archivedlog","archivedstaff","archivedstudent","consultationinfo","personalmedicalrecord","systemlogs","useraccounts");
-
- foreach($tableArray as $table)
- {
-  $show_table_query = "SHOW CREATE TABLE " . $table . "";
-  $statement = $connectPDO->prepare($show_table_query);
-  $statement->execute();
-  $show_table_result = $statement->fetchAll();
-
-  foreach($show_table_result as $show_table_row)
-  {
-   $output .= "\n\n" . $show_table_row["Create Table"] . ";\n\n";
-  }
-  $select_query = "SELECT * FROM " . $table . "";
-  $statement = $connectPDO->prepare($select_query);
-  $statement->execute();
-  $total_row = $statement->rowCount();
-
-  for($count=0; $count<$total_row; $count++)
-  {
-   $single_result = $statement->fetch(PDO::FETCH_ASSOC);
-   
-
-   $table_column_array = array_keys($single_result);
-   $table_value_array = array_values($single_result);
-   if($table == "personalmedicalrecord" || $table == "archivedstudent"){
-    $table_value_array[6] = null;
-   }
+    $tables = '*';
+    $return = '';
+  
+    //Call the core function
+    backup_tables($Server, $User, $DBPassword, $Database, $tables);
 
 
-   $output .= "\nINSERT INTO $table (";
-   $output .= "" . implode(", ", $table_column_array) . ") VALUES (";
-   $output .= "'" . implode("','", $table_value_array) . "');\n";
-  }
- }
- $file_name = $TxtFileName ."--" . date('Y-m-d') . '.sql';
- $file_handle = fopen($file_name, 'w+');
- fwrite($file_handle, $output);
- fclose($file_handle);
- header('Content-Description: File Transfer');
- header('Content-Type: application/octet-stream');
- header('Content-Disposition: attachment; filename=' . basename($file_name));
- header('Content-Transfer-Encoding: binary');
- header('Expires: 0');
- header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($file_name));
-    ob_clean();
-    flush();
-    readfile($file_name);
-    unlink($file_name);
+}
+
+//Core function
+function backup_tables($host, $user, $pass, $dbname, $tables = '*') {
+    global $return; 
+    $link = mysqli_connect($host,$user,$pass, $dbname);
+
+    // Check connection
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit;
+    }
+
+    mysqli_query($link, "SET NAMES 'utf8'");
+
+    //get all of the tables
+    if($tables == '*')
+    {
+        $tables = array();
+        $result = mysqli_query($link, 'SHOW TABLES');
+        while($row = mysqli_fetch_row($result))
+        {
+            $tables[] = $row[0];
+        }
+    }
+    else
+    {
+        $tables = is_array($tables) ? $tables : explode(',',$tables);
+    }
+
+    $TxtFileName = $_POST['TxtFileName'];
+    $return.= "\n\nDROP DATABASE if exists clinicRecord;\n";
+    $return.= "CREATE DATABASE clinicRecord;\n";
+    $return.= "USE clinicRecord;\n";
+
+    
+    //cycle through
+    foreach($tables as $table)
+    {
+        $result = mysqli_query($link, 'SELECT * FROM '.$table);
+        $num_fields = mysqli_num_fields($result);
+        $num_rows = mysqli_num_rows($result);
+
+        $return.= 'DROP TABLE IF EXISTS '.$table.';';
+        $row2 = mysqli_fetch_row(mysqli_query($link, 'SHOW CREATE TABLE '.$table));
+        $return.= "\n\n".$row2[1].";\n\n";
+        $counter = 1;
+
+        //Over tables
+        for ($i = 0; $i < $num_fields; $i++) 
+        {   //Over rows
+            while($row = mysqli_fetch_row($result))
+            {   
+                if($counter == 1){
+                    $return.= 'INSERT INTO '.$table.' VALUES(';
+                } else{
+                    $return.= '(';
+                }
+
+                //Over fields
+                for($j=0; $j<$num_fields; $j++) 
+                {
+                    $row[$j] = addslashes($row[$j]);
+                    $row[$j] = str_replace("\n","\\n",$row[$j]);
+                    if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+                    if ($j<($num_fields-1)) { $return.= ','; }
+                }
+
+                if($num_rows == $counter){
+                    $return.= ");\n";
+                } else{
+                    $return.= "),\n";
+                }
+                ++$counter;
+            }
+        }
+        $return.="\n\n\n";
+    }
+
+    //save file
+    $fileName = $TxtFileName ." (" . date('M-d-Y') .'--' . date('h.i A') . ').sql';
+    $handle = fopen($fileName,'w+');
+    fwrite($handle,$return);
+    if(fclose($handle)){
+        echo "Done! Check your backup in Downloads folder with filename: ".$fileName;
+
+
+//        echo "\n\nDone, Check your backup at your Downloads folder with filename: ".$fileName;
+         header('Content-Description: File Transfer');
+         header('Content-Type: application/octet-stream');
+         header('Content-Disposition: attachment; filename=' . basename($fileName));
+         header('Content-Transfer-Encoding: binary');
+         header('Expires: 0');
+         header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($fileName));
+            ob_clean();
+            flush();
+            readfile($fileName);            
+            unlink($fileName);
+        exit; 
+    }
 }
 
 
